56a5aeeee850ba8c7436768698840079ba92cc55
sizh-it-solutions <info@sizhitsolutions.com>
2026-04-27 21:53:14 +0530

    "userdetails"

--- src/main/java/com/vrijeshwar/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/vrijeshwar/security/service/UserDetailsServiceImpl.java
@@ -15,6 +15,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
   @Autowired
   UserRepository userRepository;
 
+
   @Override
   @Transactional
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
